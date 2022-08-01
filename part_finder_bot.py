79f9c50de3a00401b931334dc12b11866183db34
DTF001 <yegor.vershinin@gmail.com>
2022-08-01 20:44:48 +0300

    key file added

--- part_finder_bot.py
+++ part_finder_bot.py
@@ -1,18 +1,12 @@
 import telebot
 import random as rdm
+import find_machine
+import dataset_vdtm
+import dataset_vd_airlines
+import lists_replies
+import bot_key
 
-from find_machine import finder, extra_finder, extractor, trax_inventory_explorer
-
-from dataset_vdtm import inv_qry_vdtm, pns_vdtm, pns_vdtm_int, pn_vdtm_category, columns_vdtm, wh_list_vdtm
-
-from dataset_vd_airlines import inv_qry_airlines, pns_airlines, pns_airlines_int, \
-    pn_airlines_category, columns_airlines, wh_list_airlines
-
-from lists_replies import r_list, ok_list, nb_list
-
-# BOT_KEY
-key = '5375777787:AAHjFAjfGquE-BtZ7kCvemee-5dbKu9dPYw'
-bot = telebot.TeleBot(key)
+bot = telebot.TeleBot(bot_key.key)
 
 
 # Функция, обрабатывающая команду /start
@@ -27,24 +21,24 @@ def start(m):
 def handle_text(m):
     requested_pn = str(m.text)
     bot.send_message(m.chat.id, 'Запрошенный p/n: ' + requested_pn)
-    bot.send_message(m.chat.id, rdm.choice(ok_list))
+    bot.send_message(m.chat.id, rdm.choice(lists_replies.ok_list))
 
     def checker(company, master, slave, your_pn, inv_qry, columns, warehouses, mat_type, precision):
         a = 0
-        found = finder(master=master, slave=slave, your_pn=your_pn)
-        result = trax_inventory_explorer(inv_qry=inv_qry, columns=columns, warehouses=warehouses, found=found)
+        found = find_machine.finder(master=master, slave=slave, your_pn=your_pn)
+        result = find_machine.trax_inventory_explorer(inv_qry=inv_qry, columns=columns, warehouses=warehouses, found=found)
         if not result.empty:
             result = result.to_string(index=False, header=False, justify='left')
             bot.send_message(m.chat.id, 'Я нашёл в ' + company + ':')
             bot.send_message(m.chat.id, str(result))
             a = 1
         else:
-            extra_found = extra_finder(slave=slave, mat_type=mat_type, precision=precision, your_pn=your_pn)
+            extra_found = find_machine.extra_finder(slave=slave, mat_type=mat_type, precision=precision, your_pn=your_pn)
             if extra_found:
-                extra_found_extracted = extractor(extra_found)
-                extra_found_extracted = finder(master=master, slave=slave, your_pn=extra_found_extracted)
-                result = trax_inventory_explorer(inv_qry=inv_qry, columns=columns, found=extra_found_extracted,
-                                                 warehouses=warehouses)
+                extra_found_extracted = find_machine.extractor(extra_found)
+                extra_found_extracted = find_machine.finder(master=master, slave=slave, your_pn=extra_found_extracted)
+                result = find_machine.trax_inventory_explorer(inv_qry=inv_qry, columns=columns, found=extra_found_extracted,
+                                                              warehouses=warehouses)
                 if not result.empty:
                     result = result.to_string(index=False, header=False, justify='left')
                     bot.send_message(m.chat.id, 'Я нашёл в ' + company + ':')
@@ -52,15 +46,15 @@ def handle_text(m):
                     a = 1
         return a
 
-    vdt = checker(company='ВДТМ', master=pns_vdtm, slave=pns_vdtm_int, your_pn=requested_pn, inv_qry=inv_qry_vdtm,
-                  columns=columns_vdtm, warehouses=wh_list_vdtm, mat_type=pn_vdtm_category, precision=75)
-    airlines = checker(company='ABC и ATRAN', master=pns_airlines, slave=pns_airlines_int, your_pn=requested_pn,
-                       inv_qry=inv_qry_airlines, columns=columns_airlines, warehouses=wh_list_airlines,
-                       mat_type=pn_airlines_category, precision=75)
+    vdt = checker(company='ВДТМ', master=dataset_vdtm.pns_vdtm, slave=dataset_vdtm.pns_vdtm_int, your_pn=requested_pn, inv_qry=dataset_vdtm.inv_qry_vdtm,
+                  columns=dataset_vdtm.columns_vdtm, warehouses=dataset_vdtm.wh_list_vdtm, mat_type=dataset_vdtm.pn_vdtm_category, precision=75)
+    airlines = checker(company='ABC и ATRAN', master=dataset_vd_airlines.pns_airlines, slave=dataset_vd_airlines.pns_airlines_int, your_pn=requested_pn,
+                       inv_qry=dataset_vd_airlines.inv_qry_airlines, columns=dataset_vd_airlines.columns_airlines, warehouses=dataset_vd_airlines.wh_list_airlines,
+                       mat_type=dataset_vd_airlines.pn_airlines_category, precision=75)
     if vdt == 0 and airlines == 0:
-        bot.send_message(m.chat.id, rdm.choice(nb_list))
+        bot.send_message(m.chat.id, rdm.choice(lists_replies.nb_list))
     else:
-        bot.send_message(m.chat.id, rdm.choice(r_list))
+        bot.send_message(m.chat.id, rdm.choice(lists_replies.r_list))
 
 
 # Запускаем бота
