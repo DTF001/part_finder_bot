aead83dcafedae4ba6de9e300acb89084f65aa85
DTF001 <yegor.vershinin@gmail.com>
2022-08-10 18:52:17 +0300

    minor changes

--- find_machine.py
+++ find_machine.py
@@ -23,7 +23,7 @@ def extra_finder(slave, master, your_pn):
         if your_pn in slave[index]:
             found_pn = master[index]
             for value in range(len(master)):
-                if found_pn == master[value]:
+                if found_pn == master[value] and master[value] not in print_list_adv:
                     print_list_adv.append(slave[value])
     print(print_list_adv)
     return print_list_adv
