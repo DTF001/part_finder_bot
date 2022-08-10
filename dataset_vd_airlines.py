77020afe9b635fe3d6a14be9764aec14e0fe3a9e
DTF001 <yegor.vershinin@gmail.com>
2022-08-10 22:11:46 +0300

    minor changes

--- dataset_vd_airlines.py
+++ dataset_vd_airlines.py
@@ -9,5 +9,4 @@ pn_airlines_category = list(pn_master_airlines['category'])
 # CATALOG_INVENTORY_AIRLINES
 inv_qry_airlines = pd.read_csv('inv_qry_airlines.csv', encoding='cp1252', low_memory=False)
 columns_airlines = ['location', 'pn', 'pn_description', 'qty_available', 'stock_uom']
-wh_list_airlines = ['FRA', 'VKO-ATR-SV', 'SHJ', 'SVO', 'FRA-ATR', 'SVO-ATRAN', 'LGG ATR', 'LEJ', 'SHJ_ATRAN', 'LGG_ABC',
-                    'UTG-ATR-SV', 'KJA-ATR-SV', 'RIX-ATR-SV', 'DME-ATR-SV', 'LEJ_ATRAN', 'ORD', 'KJA', 'PVG']
+wh_list_airlines = ['VKO-ATR-SV', 'SVO', 'SVO-ATRAN', 'UTG-ATR-SV', 'KJA-ATR-SV', 'DME-ATR-SV', 'KJA']
--- dataset_vdtm.py
+++ dataset_vdtm.py
@@ -9,4 +9,4 @@ pn_vdtm_category = list(pn_master_vdtm['category'])
 # CATALOG_INVENTORY_VDTM
 inv_qry_vdtm = pd.read_csv('inv_qry_vdtm.csv', encoding='cp1252')
 columns_vdtm = ['location', 'pn', 'pn_description', 'qty_available', 'stock_uom']
-wh_list_vdtm = ['SVO', 'VKO', 'DME', 'SVO_SALES', 'KJA']
+wh_list_vdtm = ['SVO', 'VKO', 'DME', 'SVO_SALES', 'KJA', 'SVO_TRS']
